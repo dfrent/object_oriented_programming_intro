b6121fc1ec1c92c3eaf06de459dd3502f7cfb2b9
cordell <cordell.perry@gmail.com>
2017-11-09 18:38:43 -0500

    cat class exercise

--- cat.rb
+++ cat.rb
@@ -1,7 +1,51 @@
 class Cat
 
-  
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+    @eats_at = eats_at
+  end
+#-------readers---------
+  def name
+    @name
+  end
 
+  def preferred_food
+    @preferred_food
+  end
 
+  def meal_time
+    @meal_time
+  end
+
+  def eats_at
+    @eats_at
+  end
+
+# -----writers------------
+  # def name=(name)
+  #   @name = name
+  # end
+  #
+  # def preferred_food
+  #   @preferred_food = preferred_food
+  # end
+  #
+  # def meal_time
+  #   @meal_time = meal_time
+  # end
+
+  def eats_at
+    if meal_time <= 11
+        "#{meal_time + 1}AM"
+    else
+       "#{meal_time - 11}PM"
+    end
+  end
 
 end
+
+  cat = Cat.new("fluffy", "popcorn", 23)
+
+  puts cat.eats_at
